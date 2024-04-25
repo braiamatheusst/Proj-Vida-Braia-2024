6c26015c4827e3b13de0eda0bed024ad861928d5
braia <108024615+braiamatheusst@users.noreply.github.com>
2024-04-25 17:30:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -16,8 +16,11 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-13T00:00:00");
+const tempoObjetivo2 = new Date("2025-01-13T00:00:00");
+const tempoObjetivo3 = new Date("2025-03-13T00:00:00");
+const tempoObjetivo4 = new Date("2025-06-13T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -36,3 +39,16 @@ function calculaTempo(tempoObjetivo) {
 
 
 }
+
+function atualizaCronometro(){
+    for( let i=0; i<contadores.length; i++ ){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaConometro(){
+    atualizaCronometro();
+    seltInterval(atualizaCronometro, 1000);
+}
+
+comecaConometro()
